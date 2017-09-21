29cdcd428c4a39deb6862d16ab035dadeb67d0ff
colepacak <cole@favish.com>
2017-09-21 08:46:27 -0500

    initial commit, set up webpack dev server

---
+++ js/app/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    "whatwg-fetch",
+    "webpack-dev-server/client?http://localhost:8081",
+    "webpack/hot/only-dev-server",
+    path.resolve(__dirname, "src/index.js")
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "preschool-quiz.js",
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  devtool: "source-map",
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot-loader!babel-loader'
+    }]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+};
